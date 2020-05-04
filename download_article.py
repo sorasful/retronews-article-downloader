69e07847ded7f50a702f46114fadcef7b88a71d6
Sorasful <teva.krief@hotmail.fr>
2020-05-05 00:57:17 +0200

    Add generation of image when scraping is finished

--- download_article.py
+++ download_article.py
@@ -7,57 +7,13 @@ import httpx
 from aiofiles import os
 from loguru import logger
 
+from image_utils import reconstruct_article_image
+
 
 def extract_ids_of_article_url(url: str):
     *_, id1, id2, _ = url.split('/')
 
     return id1, id2
-
-
-async def main(article_url: str):
-    id1, id2 = extract_ids_of_article_url(article_url)
-
-    dirname = f'{id1}-{id2}'
-    if not Path(dirname).exists():
-        await os.mkdir(dirname)
-
-    async with httpx.AsyncClient(timeout=None) as client:
-        # contains information like the number of pages to iterate on
-        article_metadata_url = f"https://pv5web.retronews.fr/api/document/{id1}/{id2}"
-
-        article_metadata_response = await client.get(article_metadata_url)
-        article_metadata = article_metadata_response.json()
-
-        print(article_metadata['nbPages'])
-
-        for page in range(1, article_metadata['nbPages'] + 1):
-            await download_one_article_page(client, dirname, id1, id2, page)
-            await asyncio.sleep(0.3)
-
-
-async def download_one_article_page(client, dirname, id1, id2, page):
-    """
-    Async Function used to download all image that constitue ONE page of an article. Useful if you do not want all
-    the article.
-    :param client: HTTPX client
-    :param dirname: Directory where to store the images
-    :param id1: publication_id of the article (the first ID in the end of the url)
-    :param id2: Id of the publication (the second ID at the end of the URL)
-    :param page: Number of page (starting at 1)
-    :return:
-    """
-    coroutines = []
-
-    # TODO: Optimize calls for column and rows
-    for row in range(6):
-        for column in range(6):
-            filename = f'{id1}-{id2}-{page}-{row}-{column}.jpg'
-            path_to_store = dirname / Path(filename)
-            url = f"https://pv5web.retronews.fr/api/document/{id1}/{id2}/page/{page}/tile/{column}/{row}/0"
-            coroutines.append(download_image_from_url(url=url, client=client, filename=path_to_store))
-
-    logger.info(f"Fetching images for page {page} ...")
-    await asyncio.gather(*coroutines)
 
 
 async def download_image_from_url(url: str, client, filename: str):
@@ -76,12 +32,75 @@ async def download_image_from_url(url: str, client, filename: str):
         await f.write(response.content)
 
 
+async def download_one_article_page(client, dirname, id1, id2, page):
+    """
+    Async Function used to download all image that constitue ONE page of an article. Useful if you do not want all
+    the article.
+    :param client: HTTPX client
+    :param dirname: Directory where to store the images
+    :param id1: publication_id of the article (the first ID in the end of the url)
+    :param id2: Id of the publication (the second ID at the end of the URL)
+    :param page: Number of page (starting at 1)
+    :return:
+    """
+    coroutines = []
+
+    # TODO: Optimize calls for column and rows
+    for row in range(8):
+        for column in range(8):
+            filename = f'{id1}-{id2}-{page}-{row}-{column}.jpg'
+            path_to_store = dirname / Path(filename)
+            url = f"https://pv5web.retronews.fr/api/document/{id1}/{id2}/page/{page}/tile/{column}/{row}/0"
+            coroutines.append(download_image_from_url(url=url, client=client, filename=path_to_store))
+
+    logger.info(f"Fetching images for page {page} ...")
+    await asyncio.gather(*coroutines)
+
+
+async def download_full_article(article_url):
+    id1, id2 = extract_ids_of_article_url(article_url)
+    dirname = f'{id1}-{id2}'
+
+    if not Path(dirname).exists():
+        await os.mkdir(dirname)
+
+    async with httpx.AsyncClient(timeout=None) as client:
+        # contains information like the number of pages to iterate on
+        article_metadata_url = f"https://pv5web.retronews.fr/api/document/{id1}/{id2}"
+
+        article_metadata_response = await client.get(article_metadata_url)
+        article_metadata = article_metadata_response.json()
+
+        for page in range(1, article_metadata['nbPages'] + 1):
+            await download_one_article_page(client, dirname, id1, id2, page)
+            await asyncio.sleep(0.3)
+
+            reconstruct_article_image(page_to_reconstruct=page, directory_holding_images=dirname)
+
+
+async def download_page_from_article(article_url: str, page: int):
+    id1, id2 = extract_ids_of_article_url(article_url)
+    dirname = f'{id1}-{id2}'
+
+    if not Path(dirname).exists():
+        await os.mkdir(dirname)
+
+    async with httpx.AsyncClient(timeout=None) as client:
+        # await download_one_article_page(client, dirname, id1, id2, page)
+        reconstruct_article_image(page_to_reconstruct=page, directory_holding_images=dirname)
+
+
+async def main(article_url: str):
+    await download_page_from_article(article_url, page=3)
+
+
 if __name__ == '__main__':
     # https://github.com/encode/httpx/issues/914
     if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
     article_url = "https://www.retronews.fr/journal/le-petit-marseillais/24-mars-1938/437/1806613/1"
+
     asyncio.run(main(article_url=article_url))
 
     logger.info("Program finished.")
--- image_utils.py
+++ image_utils.py
@@ -4,22 +4,25 @@ from pathlib import Path
 from PIL import Image
 
 
-def reconstruct_article_image(page_to_reconstruct:int, directory_holding_images: str):
+def reconstruct_article_image(page_to_reconstruct: int, directory_holding_images: str):
     directory_holding_images = Path(directory_holding_images)
-    images_paths_for_page = sorted([str(x) for x in filter(lambda x: re.search(f"\d+-\d+-{page_to_reconstruct}-\d+-\d+.jpg", x.name), directory_holding_images.iterdir())])
+    images_paths_for_page = sorted([str(x) for x in
+                                    filter(lambda x: re.search(f"\d+-\d+-{page_to_reconstruct}-\d+-\d+.jpg", x.name),
+                                           directory_holding_images.iterdir())])
 
     # sort the name of the images, get the last one (with biggest row and col)
     # then use regex to capture the values
-    max_col, max_row = map(int, re.search(f"\d+-\d+/\d+-\d+-{page_to_reconstruct}-(\d+)-(\d+).jpg", images_paths_for_page[-1]).groups())
+    max_col, max_row = map(int, re.search(f"\d+-\d+/\d+-\d+-{page_to_reconstruct}-(\d+)-(\d+).jpg",
+                                          images_paths_for_page[-1]).groups())
 
     im0 = Image.open(images_paths_for_page[0])
     width, height = map(int, im0.size)
 
-    result_img = Image.new('RGB', (width * max_col + 1 ,height * max_row + 1))
+    result_img = Image.new('RGB', (width * (max_row + 1), height * (max_col + 1)))
 
     for img in images_paths_for_page:
         col, row = map(int, re.search(f"\d+-\d+/\d+-\d+-{page_to_reconstruct}-(\d+)-(\d+).jpg", img).groups())
         image = Image.open(img)
-        result_img.paste(image, (row* width, col * height))
+        result_img.paste(image, (row * width, col * height))
 
     result_img.save(f'{directory_holding_images}-{page_to_reconstruct}.jpg')
