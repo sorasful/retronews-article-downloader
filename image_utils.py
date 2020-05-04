05a9ba918d5bf05627f4b449e71cac7a8d978ec8
Sorasful <teva.krief@hotmail.fr>
2020-05-04 19:20:29 +0200

    Turn image utils into a function

--- image_utils.py
+++ image_utils.py
@@ -1,28 +1,25 @@
-from PIL import Image, ImageDraw, ImageFilter
-from pathlib import Path
-import os
 import re
+from pathlib import Path
 
-directory = Path('437-1806613')
-page = 1
-
-images_paths_for_page = sorted([str(x) for x in filter(lambda x: re.search(f"\d+-\d+-{page}-\d+-\d+.jpg", x.name), directory.iterdir())])
-
-# sort the name of the images, get the last one (with biggest row and col)
-# then use regex to capture the values
-max_col, max_row = map(int, re.search(f"\d+-\d+/\d+-\d+-{page}-(\d+)-(\d+).jpg", images_paths_for_page[-1]).groups())
-
-# images = [Image.open(path) for path in images_paths_for_page]
+from PIL import Image
 
 
-im0 = Image.open(images_paths_for_page[0])
-width, height = map(int, im0.size)
+def reconstruct_article_image(page_to_reconstruct:int, directory_holding_images: str):
+    directory_holding_images = Path(directory_holding_images)
+    images_paths_for_page = sorted([str(x) for x in filter(lambda x: re.search(f"\d+-\d+-{page_to_reconstruct}-\d+-\d+.jpg", x.name), directory_holding_images.iterdir())])
 
-result_img = Image.new('RGB', (width * max_col + 1 ,height * max_row + 1))
+    # sort the name of the images, get the last one (with biggest row and col)
+    # then use regex to capture the values
+    max_col, max_row = map(int, re.search(f"\d+-\d+/\d+-\d+-{page_to_reconstruct}-(\d+)-(\d+).jpg", images_paths_for_page[-1]).groups())
 
-for img in images_paths_for_page:
-    col, row = map(int, re.search(f"\d+-\d+/\d+-\d+-{page}-(\d+)-(\d+).jpg", img).groups())
-    image = Image.open(img)
-    result_img.paste(image, (row* width, col * height))
+    im0 = Image.open(images_paths_for_page[0])
+    width, height = map(int, im0.size)
 
-result_img.save('results.jpg')
+    result_img = Image.new('RGB', (width * max_col + 1 ,height * max_row + 1))
+
+    for img in images_paths_for_page:
+        col, row = map(int, re.search(f"\d+-\d+/\d+-\d+-{page_to_reconstruct}-(\d+)-(\d+).jpg", img).groups())
+        image = Image.open(img)
+        result_img.paste(image, (row* width, col * height))
+
+    result_img.save(f'{directory_holding_images}-{page_to_reconstruct}.jpg')
